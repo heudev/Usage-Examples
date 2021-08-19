7c2137a8aa8815351e659474ec0bd0028f0c7852
Enes Uysal <74737994+hulkienesuysal@users.noreply.github.com>
2021-08-19 22:19:07 +0300

    Add files via upload

---
+++ mongoose-use.js
@@ -0,0 +1,122 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    adsoyad: String,
+    yas: Number,
+    telefon: String,
+    email: String,
+    createdAt: String,
+    updatedAt: String,
+});
+
+const Users = mongoose.model("Users", userSchema);
+module.exports = Users;
+
+//----------------------------------------------------------------------------------------------------------------
+
+const dbURL = "mongodb+srv://";
+mongoose.connect(dbURL, { useUnifiedTopology: true, useNewUrlParser: true, useFindAndModify: false, })
+    .then((result) => console.log("Baglanti kuruldu"))
+    .catch((err) => console.log(err));
+
+//----------------------------------------------------------------------------------------------------------------
+
+// Yeni kayıt ekleme.
+const User = new Users();
+User.adsoyad = "enes";
+User.telefon = "096565131321";
+User.yas = 20
+User.email = "enes@example.com";
+User.createdAt = new Date().toLocaleString();
+User.save().then((result) => {
+    console.log(result);
+});
+
+//----------------------------------------------------------------------------------------------------------------
+
+// Tüm kayıtları listeler.
+Users.find().then((result) => {
+    console.log(result);
+});
+
+// Koşullu listeleme. (and)
+Users.find({ adsoyad: "enes", yas: 20 }).then((result) => {
+    console.log(result);
+});
+
+// Belirtilern id değerine ait kaydı getirir.
+Users.findById("610448f883c7150d54433d8c").then((result) => {
+    console.log(result);
+});
+
+// İlk bulduğu kaydı getirir.
+Users.findOne({ adsoyad: "enes" }).then((result) => {
+    console.log(result);
+});
+
+// Yaşı, belirtilen değerden küçük olanları listeler.
+Users.find({ yas: { $lt: 30 } }).then((result) => {
+    console.log(result);
+});
+
+// Yaşı belirtilen değere eşit veya büyük olanları listler.
+Users.find({ yas: { $gte: 40 } }).then((result) => {
+    console.log(result);
+});
+
+// Tüm kayıtları adsoyad'a göre alfabetik olarak listeler.
+Users.find().sort({ adsoyad: 1 }).then((result) => {
+    console.log(result);
+});
+
+// Tüm kayıtları adsoyad'a göre alfabenin tesine göre listeler.
+Users.find().sort({ adsoyad: -1 }).then((result) => {
+    console.log(result);
+});
+
+// adsoyadı s harfi ile başlayanları listeler.
+Users.find({ adsoyad: { $regex: "^s" } }).then((result) => {
+    console.log(result);
+});
+
+// Belirtilen sayıda kayıt listeler.
+Users.find().limit(2).then((result) => {
+    console.log(result);
+});
+
+//----------------------------------------------------------------------------------------------------------------
+
+// Belirtilen id'deki kaydı değiştirir. (mevcut/değişecek)
+Users.findByIdAndUpdate("610448f883c7150d54433d8c", { adsoyad: "enes" }).then(
+    (result) => { }
+);
+
+// Belirtilen değerdeki kaydı, yeni verilen değer ile değiştirir. (mevcut/değişecek)
+Users.findOneAndUpdate({ adsoyad: "enes" }, { adsoyad: "james" }).then(
+    (result) => { }
+);
+
+//----------------------------------------------------------------------------------------------------------------
+
+// Belirtilen id değerine ait kaydı siler.
+Users.findByIdAndDelete("610448c0eb7f55133cbe82d6").then((result) => { });
+
+// Belirtilen değere ait kaydı siler.
+Users.findOneAndDelete({ adsoyad: "enes" }).then((result) => { });
+
+// Cluster'daki tüm kayıltarı siler.
+Users.deleteMany().then((result) => { });
+
+//----------------------------------------------------------------------------------------------------------------
+
+
+/*
+.then((result)=>{
+        console.log(result)
+    })
+.catch((err)=>{
+    console.log(err)
+})
+*/
---
+++ pymongo-use.py
@@ -0,0 +1,83 @@
+from pymongo import MongoClient
+#pip install pymongo[srv]
+
+myclient = MongoClient("mongodb+srv://")
+mydb = myclient["qwe"] #db
+mycol = mydb["qwe"] #cluster
+
+# Tüm veritabanlarını listeler
+print(myclient.list_database_names())
+
+# Tüm cluster'ları listeler
+print(mydb.list_collection_names())
+
+#---------------------------------------------------------------------
+
+# Kayıt ekleme.
+mydict = { "name": "Peter", "address": "Lowstreet 27" }
+x = mycol.insert_one(mydict)  
+print(x.inserted_id)
+
+# Birden çok kayıt ekleme.
+mylist = [
+  { "_id": 1, "name": "John", "address": "istanbul"},
+  { "_id": 2, "name": "Peter", "address": "izmir"},
+  { "name": "William", "address": "Central st 954"},
+]
+x = mycol.insert_many(mylist)
+print(x.inserted_ids)
+
+#---------------------------------------------------------------------
+
+# Tüm kayıtları listeler.
+for x in mycol.find():
+  print(x["name"])
+
+#Koşullu listeleme.
+for x in mycol.find({ "_id": 0, "name": "enes" }):
+  print(x)
+
+# Belirli sayıda kayıt listeler.
+myresult = mycol.find().limit(5)
+for x in myresult:
+  print(x)
+
+# address colonunda S ile başlayan kayıtları listeler.
+for x in mycol.find({ "address": { "$regex": "^S" } }):
+  print(x)
+
+# sonuçları name'e göre alfabetik olarak sıralar.
+mydoc = mycol.find().sort("name")
+for x in mydoc:
+  print(x)
+
+# sonuçları name'e göre alfabetik olarak ters sıralar.
+mydoc = mycol.find().sort("name", -1)
+for x in mydoc:
+  print(x)
+
+#---------------------------------------------------------------------
+
+# Belirtilen değere ait kayıtları siler.
+mycol.delete_one({ "address": "Mountain 21" })
+
+# Collection'daki tüm kayıtıları siler.
+x = mycol.delete_many({})
+print(x.deleted_count, " documents deleted.")
+
+# Collection'ı siler.
+mycol.drop()
+
+#---------------------------------------------------------------------
+
+# Belirtilen kaydı yeni kayıt ile günceller.
+myquery = { "address": "mugla" }
+newvalues = { "$set": { "address": "aydin" } }
+x = mycol.update_one(myquery, newvalues)
+print(x.modified_count, "documents updated.")
+
+# S harfi ile başlayan tüm kayıtları günceller.
+myquery = { "address": { "$regex": "^S" } }
+newvalues = { "$set": { "name": "Minnie" } }
+x = mycol.update_many(myquery, newvalues)
+print(x.modified_count, "documents updated.")
