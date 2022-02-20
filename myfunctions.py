ef67f150c4a0652b09a8a73178b4419db8174832
Enes Uysal <hulkienesuysal@gmail.com>
2022-02-20 14:56:05 +0300

    Update myfunctions.py

--- myfunctions.py
+++ myfunctions.py
@@ -39,3 +39,15 @@ def python_installed_packages():
     import pkg_resources
     installed_packages = {d.project_name: d.version for d in pkg_resources.working_set}
     print(installed_packages)
+
+def show_wifi_password():
+    import subprocess
+    data = subprocess.check_output(["netsh", "wlan", "show", "profiles"]).decode("utf-8").split("\n")
+    profiles = [i.split(":")[1][1:-1] for i in data if "All User Profile" in i]
+    for i in profiles:
+        results = subprocess.check_output(["netsh", "wlan", "show", "profile", i, "key=clear"]).decode("utf-8").split("\n")
+        results = [b.split(":")[1][1:-1] for b in results if "Key Content" in b]
+        try:
+            print("{:<30}|  {:<}".format(i, results[0]))
+        except IndexError:
+            print("{:<30}|  {:<}".format(i, ""))
